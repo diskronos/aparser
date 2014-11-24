1b7a4534e761c451be5537db5f54b58fd671827a
i.makarov <i.makarov@smartdesign.by>
2014-11-24 16:45:54 +0200

    refs #17509 : Akafi автоимпорт

--- akafi.php
+++ akafi.php
@@ -11,17 +11,38 @@ class Akafi
 	const TASKS_FILENAME = 'tasks.txt'; //'C:\Users\kirill\Desktop\Aparser1.1.89Beta\results\akaf\akafid.txt'
 	const TASKS_IDS_FILENAME = 'tasks/tasks_ids.txt'; //'C:\Users\kirill\Desktop\Aparser1.1.89Beta\results\akaf\akafid.txt'
 
-//	protected $_api_server = 'http://127.0.0.1:9092/API';
-	protected $_api_server = 'http://176.9.30.153:9092/API';
+	protected $_api_server = 'http://127.0.0.1:9092/API';
 	protected $_upload_server = 'base.parser.by';
 	protected $_username = 'base';
 	protected $_key_public = 'keys/public';
 	protected $_key_private = 'keys/private';
 	
+	protected $_country_code = 'SA';
+	protected $_source = 'aparser-A';
+	
 	protected $_file_lock = NULL;
 	
 	public function __construct(){}
 	
+	protected static function arr_get($array, $key, $default = NULL)
+	{
+		if (!is_array($array))
+		{
+			$array = array();
+		}
+		return isset($array[$key]) ? $array[$key] : $default;
+	}
+	
+	protected static function clear_arr($array)
+	{
+		if (!is_array($array))
+		{
+			$array = array();
+		}
+		$array = array_map('trim', $array);
+		return array_diff($array, array('', NULL));
+	}
+
 	protected function set_lock($lockname)
 	{
 		$this->_file_lock = fopen('locks/' . $lockname . '.lock', 'w');
@@ -30,19 +51,55 @@ class Akafi
 			die('Already in action');
 		}
 	}
+
+	protected function open_and_lock_file($filename)
+	{
+		$file = fopen($filename, 'r+');
+		flock($file, LOCK_EX);
+		$filesize = filesize($filename);
+		if (!$filesize)
+		{
+			return FALSE;
+		}
+		return $file;
+	}
 	
+	protected function rewrite_and_close_file($file, $data)
+	{
+		fseek($file, 0);
+		ftruncate($file, 0);
+		fwrite($file, $data);
+		fclose($file);
+	}
+
 	public function set_task($count = 1000)
 	{
 		$this->set_lock('set_task');
 
-		$file = file(self::TASKS_FILENAME);
-		rsort($file, SORT_NUMERIC);
-		$idmax = trim($file[0]);
-		$idmin = $idmax - $count;
+		$tasks_file = $this->open_and_lock_file(self::TASKS_FILENAME);
+		if (!$tasks_file)
+		{
+			return FALSE;
+		}
+
+		$akafi_tasks_ids = fread($tasks_file, filesize(self::TASKS_FILENAME));
+		$akafi_tasks_ids = explode("\n", $akafi_tasks_ids);
+		$akafi_tasks_ids = self::clear_arr($akafi_tasks_ids);
+
+		rsort($akafi_tasks_ids, SORT_NUMERIC);
+		$min_id_key  = (count($akafi_tasks_ids) > 1000 ? 999 : end($akafi_tasks_ids));
+
+		$min_id = $akafi_tasks_ids[$min_id_key];
+		$max_id = $akafi_tasks_ids[0];
+
 		$aparser = new Aparser($this->_api_server, '', array('debug'=>'true'));
 		$aparser->ping();
 		$aparser->info();
-		$task_id = $aparser->addTask('default', 'akafi.net', 'text', 'https://www.akafi.net/akfnew/Sub-{num:'.$idmin.':'.$idmax.'}.html');
+		$task_id = $aparser->addTask('default', 'akafi.net', 'text', 'https://www.akafi.net/akfnew/Sub-{num:'.$min_id.':'.$max_id.'}.html');
+		
+		$akafi_tasks_ids = array_slice($akafi_tasks_ids, $min_id_key);
+		$this->rewrite_and_close_file($tasks_file, implode("\n", $akafi_tasks_ids));
+
 		$tasks_file = fopen(self::TASKS_IDS_FILENAME, "a+");
 		flock($tasks_file, LOCK_EX);
 		fwrite($tasks_file, $task_id . "\n");
@@ -53,10 +110,16 @@ class Akafi
 	{
 		$this->set_lock('get_results');
 
-		$tasks_file = fopen(self::TASKS_IDS_FILENAME, "r+");
-		flock($tasks_file, LOCK_EX);
+		$tasks_file = $this->open_and_lock_file(self::TASKS_IDS_FILENAME);
+		if (!$tasks_file)
+		{
+			return FALSE;
+		}
 
-		$tasks_ids = explode("\n", fread($tasks_file, filesize(self::TASKS_IDS_FILENAME)));
+		$tasks_file_content =  fread($tasks_file, filesize(self::TASKS_IDS_FILENAME));
+
+		$tasks_ids = explode("\n", $tasks_file_content);
+		$tasks_ids = self::clear_arr($tasks_ids);
 
 		foreach ($tasks_ids as $key => $task_id)
 		{
@@ -83,25 +146,24 @@ class Akafi
 
 			if (!$response)
 			{
+				continue;
 				return FALSE;
 			}
-			$results_file = self::RESULTS_DIR . '/task_' . $task_id . '.xml';
 			$response = json_decode($response, true);
-			file_put_contents($results_file, $response['data']);
+			if (!self::arr_get($response, 'success'))
+			{
+				continue;
+			}
+			$results_file = self::RESULTS_DIR . '/task_' . $task_id . '.xml';
+			file_put_contents($results_file, file_get_contents(self::arr_get($response, 'data')));
 			unset($tasks_ids[$key]);
 		}
 
-		fseek($tasks_file, 0);
-		ftruncate($tasks_file, filesize(self::TASKS_IDS_FILENAME));
-		fwrite($tasks_file, implode("\n", $tasks_ids) . 'test');
-		fclose($tasks_file);
+		$this->rewrite_and_close_file($tasks_file, implode("\n", $tasks_ids));
 	}
 
 	public function upload_files($files = array())
 	{
-		$source = 'aparser';
-		$country_code = 'SA';
-
 		$this->set_lock('upload_files');
 		$files = array_diff(scandir(self::RESULTS_DIR), array('.', '..'));
 
@@ -125,7 +187,7 @@ class Akafi
 			ssh2_scp_send(
 					$connection, 
 					self::RESULTS_DIR . '/' . $file, 
-					'/srv/base/test/[' .$country_code. ']_[' . $source . ']_' . $file, 
+					'/srv/base/app/upload/import/xml/person/[' .$this->_country_code. ']_[' . $this->_source . ']_' . $file, 
 					0777);
 			unlink(self::RESULTS_DIR . '/' . $file);
 		}
